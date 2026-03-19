7f75a0cd318142fe29511502bab19cd6ec5ece63
cyachumps <cyachumpss@gmail.com>
2026-03-19 15:21:14 +0300

    New script

---
+++ filecopy.ps1
@@ -0,0 +1,13 @@
+﻿
+$path = "C:\Users\u0785\AppData\Roaming\Vipaks\Domination Client\Profiles\"
+$file = "nuser37.profile"
+$index = 1
+
+$file2 = "nuser37.8DE85AE48DCEB61"
+
+
+for ($index = 1; $index -le 50; $index ++) {
+	$output = "nuser${index}.profile"
+	Copy-Item ($path + $file)  ($path + $output)
+
+}
